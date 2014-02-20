45e3348bc32ce60b00c2070430ca5a79887f5b5f
Stefan Fodor <xtephan@gmail.com>
2014-02-20 20:47:32 +0100

    proof of concept working

---
+++ controllers/login.php
@@ -0,0 +1,32 @@
+<?php 
+
+defined('C5_EXECUTE') or die("Access Denied."); 
+class LoginController extends Concrete5_Controller_Login { 
+	
+	/**
+	* Do login. Check the token authenticity
+	* and callback to parent for password verification
+	*/
+	public function do_login() {
+	
+		$vs = Loader::helper('validation/strings');
+	
+		if ( !$vs->notempty($this->post('uToken')) ) {
+				throw new Exception(t('A token is required.'));
+		}
+
+		//load the Google Authenticator class
+		Loader::library( 'google_authenticator', 'two_factor_authentication' );
+		
+		$valid_token = GoogleAuthenticator::validateToken( 'AXJBOWJQLXD45LPV', $this->post('uToken')  );
+		
+		//if toke not valid, throw Exception
+		if( $valid_token == false ) {
+			throw new Exception(t('Token expired/invalid.'));
+		}
+		
+		//let C5 do its magic to authenticate the user
+		parent::do_login();
+	}
+	
+}
